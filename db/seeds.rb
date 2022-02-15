c4159b1497442faadd5c529036b0637bc1f0e29c
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-02-15 16:09:41 +0100

    end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+p 'Delete seed'
+Article.destroy_all
+
+p 'seeding...........'
+10.times {
+  Article.create(title: Faker::Book.title, content: Faker::Quotes::Shakespeare.romeo_and_juliet_quote )
+  p '1 article is create ........'
+}
+p 'finish ..........'
